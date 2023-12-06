9a68033315a80c299197f2bc2c7d186ed0883ef3
Lucas Lobo <63929563+H4x0rModdz@users.noreply.github.com>
2023-12-06 13:26:06 -0300

    Patch 13.24

--- Offsets.cs
+++ Offsets.cs
@@ -41,4 +41,23 @@ buffEnd: 0x1C
 buffInfo: 0x10
 buffName: 0x8
 IsLaneMinion: 0xE3
+  // I don't know the names of these but LeagueDumper found this... Don't trust 100% last 16 offsets
+  // code
+null: 48 8B 05 ? ? ? ? 8B 58 0C // 0x221C4E8
+null: 48 8B 3D ? ? ? ? 48 3B CF // 0x223A0B8
+null: 48 8B 05 ? ? ? ? 48 8B 78 08 8B 40 10 4C 8D 3C C7 49 3B FF // 0x221C620
+null: 48 8B 0D ? ? ? ? 48 69 D0 ? ? 00 00 48 8B 05 // 0x221F5C0
+null: 48 89 0D ? ? ? ? 48 8D 05 ? ? ? ? 33 D2 // 0x221F590
+null: 48 89 0D ? ? ? ? 48 8D 05 ? ? ? ? 33 D2 48 89 01 48 8D 05 ? ? ? ? // 0x221F590
+null: 48 8B 0D ? ? ? ? FF 15 ? ? ? ? 48 8B 05 ? ? ? ? // 0x223A098
+null: 48 8D 8B ? ? ? ? 48 89 44 24 ? C7 44 24 // 0x3610
+null: 40 38 BB ? ? ? ? 0F 85 ? ? ? ? 66 C7 83 ? ? ? ? ? ? 0F 31 48 C1 E2 20 4C 8D 83 ? ? ? ? 48 0B C2 44 8B CF 48 89 44 24 ? 8B D7 // 0x127C
+null: 48 8B 8F ? ? 00 00 45 33 C0 8B D3 48 8B 01 FF 90 ? ? 00 00 // 0xEDF3A0
+null: 48 8D BB ? ? ? ? C6 83 ? ? ? ? ? 0F 84 // 0x1C0
+null: E8 ? ? ? ? 41 8B 97 ? ? ? ? 48 8B 0D ? ? ? ? // 0x1A3C50
+null: 88 54 24 10 53 55 56 57 41 54 41 55 41 56 41 // 0x18D9F0
+null: E8 ? ? ? ? 8B 57 44 // 0xEDD810
+null: E8 ? ? ? ? 0F 57 DB 4C 8B C0 F3 0F 5A DE // 0xD58690
+null: E8 ? ? ? ? 4C 3B F8 0F 94 C0 // 0x208AB0
+
   }
